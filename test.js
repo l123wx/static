25caf9ed0d60e900cb7fc9bb02e2b0806d3d29ea
l123wx <48666585+l123wx@users.noreply.github.com>
2022-04-26 15:01:22 +0800

    update

--- test.js
+++ test.js
@@ -1,1 +1,50 @@
-alert(123)
+function initGa {
+  return new Promise((resolve, reject) => {
+    try {
+      var GTAG_ID = 'G-HP0GTQL69G'; 
+      (function (i, s, o, g, r, a, m) {
+         i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+           (i[r].q = i[r].q || []).push(arguments)
+         }, i[r].l = 1 * new Date(); a = s.createElement(o),
+           m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m); a.onload = resolve;
+       })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+      ga('create', GTAG_ID, 'auto');
+    } catch (err) {
+      reject(err)
+    }
+  }})
+}
+
+(function() {
+  if (!ga) {
+    await initGa()
+  }
+
+  ga(function (tracker) {
+    clientId = tracker.get('clientId');
+  }
+
+  var user_event = {
+      "eventType":  "home-page-view",
+      "visitorId": clientId,
+   }
+  record_user_event(user_event, options);
+
+  function record_user_event(user_event, options) {
+     var _gre = _gre || [];
+     // Credentials for project.
+     _gre.push(['apiKey', "AIzaSyA0ZjTxdMqZjOPyWgI3DlI0Myq8tVlHPWA"]);
+     _gre.push(['logEvent', user_event]);
+     _gre.push(['projectId', 'shopai001']);
+     _gre.push(['locationId', 'global']);
+     _gre.push(['catalogId', 'default_catalog']);
+     window._gre = _gre;
+     (function () {
+       var gre = document.createElement('script')
+       gre.type = 'text/javascript'; gre.async = true;
+       gre.src = 'https://www.gstatic.com/retail/v2_event.js';
+       var s = document.getElementsByTagName('script')[0];
+       s.parentNode.insertBefore(gre, s);
+     })();
+   }
+})()
