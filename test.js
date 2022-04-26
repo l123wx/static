8208bc9803a708e0b8906457288532833fecf23f
l123wx <48666585+l123wx@users.noreply.github.com>
2022-04-26 16:54:23 +0800

    Update test.js

--- test.js
+++ test.js
@@ -11,38 +11,38 @@ function initGa(cb) {
   }
 }
 
+function getClientId(cb) {
+  window.ga(function (tracker) {
+    clientId = tracker.get('clientId');
+    cb(clientId)
+  })
+}
+
+function record_user_event(eventType, visitorId) {
+  return new Promise(resolve => {
+    var _gre = _gre || [];
+    // Credentials for project.
+    _gre.push(['apiKey', "AIzaSyA0ZjTxdMqZjOPyWgI3DlI0Myq8tVlHPWA"]);
+    _gre.push(['logEvent', {eventType, visitorId}]);
+    _gre.push(['projectId', 'shopai001']);
+    _gre.push(['locationId', 'global']);
+    _gre.push(['catalogId', 'default_catalog']);
+    window._gre = _gre;
+    (function () {
+      var gre = document.createElement('script')
+      gre.type = 'text/javascript'; gre.async = true;
+      gre.src = 'https://www.gstatic.com/retail/v2_event.js';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(gre, s);
+      s.onload = resolve;
+    })();
+  })
+}
+
 (function() {
   initGa(() => {
     getClientId(clientId => {
       record_user_event("home-page-view", clientId);
     })
   })
-  
-  function getClientId(cb) {
-    window.ga(function (tracker) {
-      clientId = tracker.get('clientId');
-      cb(clientId)
-    })
-  }
-
-  function record_user_event(eventType, visitorId) {
-    return new Promise(resolve => {
-      var _gre = _gre || [];
-      // Credentials for project.
-      _gre.push(['apiKey', "AIzaSyA0ZjTxdMqZjOPyWgI3DlI0Myq8tVlHPWA"]);
-      _gre.push(['logEvent', {eventType, visitorId}]);
-      _gre.push(['projectId', 'shopai001']);
-      _gre.push(['locationId', 'global']);
-      _gre.push(['catalogId', 'default_catalog']);
-      window._gre = _gre;
-      (function () {
-        var gre = document.createElement('script')
-        gre.type = 'text/javascript'; gre.async = true;
-        gre.src = 'https://www.gstatic.com/retail/v2_event.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(gre, s);
-        s.onload = resolve;
-      })();
-    })
-  }
 })()
