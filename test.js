62a0ca493ea43b5dbf8fbf4b9428471334d07753
liaowenxi <1219585136@qq.com>
2022-04-26 20:23:51 +0800

    update

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 console.log(Shopline.uri)
 console.log(Shopline)
 
+(function() {
+  initGa(() => {
+    // initEvent(Shopline.uri.alias)
+  })
+})()
+
 function initGa(cb) {
   if (!window.ga) {
     var GTAG_ID = 'G-HP0GTQL69G'; 
@@ -14,19 +20,60 @@ function initGa(cb) {
   }
 }
 
-function getClientId(cb) {
-  window.ga(function (tracker) {
-    clientId = tracker.get('clientId');
-    cb(clientId)
+function initEvent(type) {
+  switch(type) {
+    case 'index': indexEvent();
+    break;
+    case 'ProductsDetail': productEvent();
+    break;
+  }
+}
+
+function indexEvent() {
+  record_user_event("home-page-view");
+}
+
+function productsDetailEvent() {
+  record_user_event("detail-page-view",
+    {
+      "productDetails": [
+        {
+          "product": {
+            "id": '{{product.id}}'
+          }
+        }
+      ]
+    }
+  )
+}
+
+function eventLog(eventName) {
+  Shopline.event.on(eventName, function() {
+    console.log(eventName, arguments)
   })
 }
 
-function record_user_event(eventType, visitorId) {
-  return new Promise(resolve => {
+Object.keys(Shopline.event._caches).forEach(item => eventLog(item))
+
+
+// TODO: params 应该放哪
+function record_user_event(eventType, params = {}) {
+  let clientId;
+
+  if (!clientId) {
+    getClientId(function(_clientId) {
+      event(_clientId)
+    })
+    return
+  }
+
+  event(clientId)
+
+  function event(clientId) {
     var _gre = _gre || [];
     // Credentials for project.
     _gre.push(['apiKey', "AIzaSyA0ZjTxdMqZjOPyWgI3DlI0Myq8tVlHPWA"]);
-    _gre.push(['logEvent', {eventType, visitorId}]);
+    _gre.push(['logEvent', {eventType, clientId, ...params}]);
     _gre.push(['projectId', 'shopai001']);
     _gre.push(['locationId', 'global']);
     _gre.push(['catalogId', 'default_catalog']);
@@ -37,36 +84,39 @@ function record_user_event(eventType, visitorId) {
       gre.src = 'https://www.gstatic.com/retail/v2_event.js';
       var s = document.getElementsByTagName('script')[0];
       s.parentNode.insertBefore(gre, s);
-      s.onload = resolve;
     })();
+  }
+}
+
+function getClientId(cb) {
+  window.ga(function (tracker) {
+    clientId = tracker.get('clientId');
+    cb(clientId)
   })
 }
 
-(function() {
-  initGa(() => {
-    getClientId(clientId => {
-      record_user_event("home-page-view", clientId);
-    })
-  })
-})()
 
 // <!-- home-page-view -->
 // {% if request.page_type=='index' %}
 // <script>
-//   record_tag('home-page-view', {})
+//   record_tag({
+//   "eventType": "home-page-view",
+//   "visitorId": "visitor-id",
+// })
 // </script>
 // {% endif %}
 
 // <!-- detail-page-view -->
 // {% if request.page_type=='product' %}
 // <script>
-//   record_tag('detail-page-view', {
-//     "productDetails": [{
-//       "product": {
-//         "id": '{{product.id}}'
-//       }
-//     }]
-//   })
+//   record_tag({
+  // "eventType": "detail-page-view",
+  // "visitorId": "visitor-id",
+  // "productDetails": [{
+  //   "product": {
+  //     "id": "product-id"
+  //   }
+  // })
 // </script>
 // {% endif %}
 
@@ -74,6 +124,7 @@ function record_user_event(eventType, visitorId) {
 // {% if request.page_type=='collection' %}
 // <script>
 //   record_tag('category-page-view', {
+//   "pageCategories": ["category1 > category2"]
 //     "pageCategories": ["Jewelry"]//["{{ collection.current_type | downcase }}"]
 //   })
 // </script>
