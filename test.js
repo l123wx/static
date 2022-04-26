884757f9c0c2b0c0a3b975569117c426f9c0fe1a
l123wx <48666585+l123wx@users.noreply.github.com>
2022-04-26 16:50:47 +0800

    1

--- test.js
+++ test.js
@@ -1,49 +1,36 @@
-function initGa() {
-  return new Promise((resolve, reject) => {
-    try {
-      var GTAG_ID = 'G-HP0GTQL69G'; 
-      (function (i, s, o, g, r, a, m) {
-         i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-           (i[r].q = i[r].q || []).push(arguments)
-         }, i[r].l = 1 * new Date(); a = s.createElement(o),
-           m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m); a.onload = resolve;
-       })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-      window.ga('create', GTAG_ID, 'auto');
-    } catch (err) {
-      reject(err)
-    }
-  })
+function initGa(cb) {
+  if (!window.ga) {
+    var GTAG_ID = 'G-HP0GTQL69G'; 
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+          (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date(); a = s.createElement(o),
+          m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m); a.onload = cb;
+      })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+    window.ga('create', GTAG_ID, 'auto');
+  }
 }
 
-(async function() {
-  console.log(window.ga)
-  if (!window.ga) {
-    await initGa()
-    console.log(window.ga)
-  }
+(function() {
+  initGa(() => {
+    getClientId(clientId => {
+      record_user_event("home-page-view", clientId);
+    })
+  })
   
-  function getClientId() {
-    return new Promise(resolve => {
-        window.ga(function (tracker) {
-          clientId = tracker.get('clientId');
-          resolve(clientId)
-        })
+  function getClientId(cb) {
+    window.ga(function (tracker) {
+      clientId = tracker.get('clientId');
+      cb(clientId)
     })
   }
 
-  var user_event = {
-    "eventType":  "home-page-view",
-    "visitorId": await getClientId(),
-  }
-  
-  record_user_event(user_event);
-
-  function record_user_event(user_event, options) {
+  function record_user_event(eventType, visitorId) {
     return new Promise(resolve => {
       var _gre = _gre || [];
       // Credentials for project.
       _gre.push(['apiKey', "AIzaSyA0ZjTxdMqZjOPyWgI3DlI0Myq8tVlHPWA"]);
-      _gre.push(['logEvent', user_event]);
+      _gre.push(['logEvent', {eventType, visitorId}]);
       _gre.push(['projectId', 'shopai001']);
       _gre.push(['locationId', 'global']);
       _gre.push(['catalogId', 'default_catalog']);
