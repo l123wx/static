7f0564b76fb63053a601306da65d8c4d6d637827
l123wx <1219585136@qq.com>
2022-05-06 11:02:08 +0800

    update

--- predict.js
+++ predict.js
@@ -25,7 +25,8 @@ function initGa(cb) {
 function initPredict() {
   console.log('initPredict')
   getClientId(async function(clientId) {
-    const productList = await getPredictList('home-page-view', clientId)
+    let productList = await getPredictList('home-page-view', clientId)
+    productList = productList.slice(0, 4)
     removeProductList()
     productList.forEach(item => {
       appendProduct(item)
@@ -42,95 +43,104 @@ function getPredictList(eventType, visitorId) {
       },
       body: JSON.stringify({
         eventType,
-        visitorId
+        visitorId,
+        shop: 'g4freegear'
       })
     })
     .then(async response => {
-      resolve((await response.json())?.data?.data || [])
+      resolve((await response.json())?.data || [])
     })
   })
 }
 
 
 function appendProduct(productInfo) {
-  const productListDom = document.querySelector('.stage-featured-collection--scroll-container')
-  productListDom.innerHTML += `
-    <div class="col">
-      <a
-        data-id="${productInfo.id}"
-        data-item-no="N21F072A"
-        data-sku-id="${productInfo.variants.id}"
-        data-index=""
-        data-status="${productInfo.status === 'active'}"
-        data-name="${productInfo.title}"
-        data-price="${productInfo.price}"
-        class="product-item"
-        href="/products/${productInfo.title.replaceAll(' ', '-').toLowerCase()}"
-        data-plugin-product-item-a=""
-      >
-        <div class="product-item-image-wrapper">
-          <div class="product-item-next-image" data-plugin-product-item-next-img-box="">
-            <div data-test="" class="product-process-image " style="opacity: 1;" data-plugin-product-item-img-ele="">
-              <img
-                style="object-fit: contain; height: auto;"
-                class="lozad lazyloaded"
-                sizes="(max-width: 749px) 80vw,(max-width: 959px) 100vw"
-                src="${productInfo.images[0].src}?w=999&amp;h=999&amp;t=webp"
-              />
+  console.log(productInfo)
+  const productListDoms = document.querySelectorAll('#shopline-section-featured-collection .stage-featured-collection--scroll-container  ')
+  console.log(productListDoms)
+  productListDoms.forEach(item => {
+    const { price, compare_at_price } = productInfo.variants[0]
+    const savePrice = Math.floor((productInfo.variants[0].compare_at_price * 1 - productInfo.variants[0].price * 1) * 100) / 100
+    item.innerHTML += `
+      <div class="col">
+        <a
+          data-id="${productInfo.id}"
+          data-item-no="${productInfo.variants[0].sku}"
+          data-sku-id="${productInfo.variants[0].id}"
+          data-index=""
+          data-status="${productInfo.status === 'active'}"
+          data-name="${productInfo.title}"
+          data-price="${price * 100}"
+          class="product-item"
+          href="/products/${productInfo.handle}"
+          data-plugin-product-item-a=""
+        >
+          <div class="product-item-image-wrapper">
+            <div class="product-item-next-image" data-plugin-product-item-next-img-box="">
+              <div data-test="" class="product-process-image " style="opacity: 1;" data-plugin-product-item-img-ele="">
+                <img
+                  style="object-fit: contain; height: auto;"
+                  class="lozad lazyloaded"
+                  sizes="(max-width: 749px) 80vw,(max-width: 959px) 100vw"
+                  src="${productInfo.image.src}?w=999&amp;h=999&amp;t=webp"
+                />
+              </div>
             </div>
-          </div>
-          <div class="product-item-image" data-plugin-product-item-img-box="">
-            <div data-test="" class="product-process-image" style="opacity: 1;" data-plugin-product-item-img-ele="">
-              <img
-                style="object-fit: contain; height: auto;"
-                class="lozad lazyloaded"
-                sizes="(max-width: 749px) 80vw,(max-width: 959px) 100vw"
-                src="${productInfo.images[1].src}?w=1500&amp;h=1500&amp;t=webp"
+            <div class="product-item-image" data-plugin-product-item-img-box="">
+              <div data-test="" class="product-process-image" style="opacity: 1;" data-plugin-product-item-img-ele="">
+                <img
+                  style="object-fit: contain; height: auto;"
+                  class="lozad lazyloaded"
+                  sizes="(max-width: 749px) 80vw,(max-width: 959px) 100vw"
+                  src="${productInfo.image.src}?w=1500&amp;h=1500&amp;t=webp"
+                >
+              </div>
+            </div>
+            <span class="product-item-sale-tag body4">Sale</span>
+            <div class="product-item-btn-con d-none d-md-block">
+              <button
+                data-query=""
+                data-spu-seq="${productInfo.id}"
+                data-unique-key="${productInfo.handle}"
+                class="btn btn-primary btn-sm product-item-btn"
+                type="button"
               >
+                Quick view
+              </button>
             </div>
           </div>
-          <span class="product-item-sale-tag body4">Sale</span>
-          <div class="product-item-btn-con d-none d-md-block">
-            <button
-              data-query=""
-              data-spu-seq="${productInfo.id}"
-              data-unique-key="${productInfo.title.replaceAll(' ', '-').toLowerCase()}"
-              class="btn btn-primary btn-sm product-item-btn"
-              type="button"
-            >
-              Quick view
-            </button>
+          <div class="product-item-info">
+            <div class="product-item-title product-grid-font">
+              ${productInfo.title}
+            </div>
+            <div data-ssr-product-item-price-top=""></div>
+            <div class="product-item-price body-font display-center">
+              <!-- 原价 -->
+              <span data-product-item-price="${compare_at_price * 100}" class="product-item-origin-price notranslate">$${compare_at_price}</span>
+              <span class="product-item-sale-price">
+                <!-- 现价 -->
+                <span data-product-item-price="${price * 100}" data-from="1">From $${price}</span>
+              </span>
+              <span class="product-item-save-price">
+                Save
+                <!-- 节省 -->
+                <span class="notranslate" data-product-item-price="${savePrice * 100}">$${savePrice}</span>
+              </span>
+            </div>
+            <div data-ssr-product-item-price-bottom=""></div>
           </div>
-        </div>
-        <div class="product-item-info">
-          <div class="product-item-title product-grid-font">
-            ${productInfo.title}
-          </div>
-          <div data-ssr-product-item-price-top=""></div>
-          <div class="product-item-price body-font display-center">
-            <!-- 原价 -->
-            <span data-product-item-price="${productInfo.compare_at_price}" class="product-item-origin-price notranslate">$${productInfo.compare_at_price / 100}</span>
-            <span class="product-item-sale-price">
-              <!-- 现价 -->
-              <span data-product-item-price="${productInfo.price}" data-from="1">From $${productInfo.price / 100}</span>
-            </span>
-            <span class="product-item-save-price">
-              Save
-              <!-- 节省 -->
-              <span class="notranslate" data-product-item-price="${productInfo.compare_at_price - productInfo.price}">$${(productInfo.compare_at_price - productInfo.price) / 100}</span>
-            </span>
-          </div>
-          <div data-ssr-product-item-price-bottom=""></div>
-        </div>
-        <div data-ssr-product-item-bottom=""></div>
-      </a>
-    </div>
-  `
+          <div data-ssr-product-item-bottom=""></div>
+        </a>
+      </div>
+    `
+  })
 }
 
 function removeProductList() {
-  const productListDom = document.querySelector('.stage-featured-collection--scroll-container')
-  productListDom.innerHTML = ''
+  const productListDoms = document.querySelectorAll('#shopline-section-featured-collection .stage-featured-collection--scroll-container  ')
+  productListDoms.forEach(item => {
+    item.innerHTML = ''
+  })
 }
 
 function getClientId(cb) {
